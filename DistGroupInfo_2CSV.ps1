403a62fd45a93be00b74b0922833d2048f2d0a8b
LiquidMovement <53025867+LiquidMovement@users.noreply.github.com>
2019-07-20 19:42:52 -0500

    Add files via upload

---
+++ DistGroupInfo_2CSV.ps1
@@ -0,0 +1,24 @@
+﻿$states = import-csv -path "C:\temp\LO_ST\States.csv" -header ST
+
+foreach($state in $states){
+    $st = $state.ST
+    $info = Get-ADGroup -Identity $st -Properties mail,displayname,members
+    $mail = $info.mail
+    $members = $info.Members
+    $grpType = $info.GroupCategory
+
+    $stateFull = Read-Host "Enter State for $st"
+
+    "State : $stateFull" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+    "Name : $st" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+    "Email : $mail" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+    "Group Type : $grpType" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+    "Members :" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+
+    foreach($user in $members){
+        $user = Get-ADUser -Identity $user -Properties UserPrincipalName | Select-Object UserPrincipalName
+        $upn = $user.UserPrincipalName
+        $upn | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+    }
+    "`n" | out-file "C:\temp\LO_ST\DistGroups.csv" -Append
+}
